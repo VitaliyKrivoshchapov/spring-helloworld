3cb3541cc08931bf3ec455fa13d5b6737e5ddbee
VitaliyKrivoshchapov <69453241+VitaliyKrivoshchapov@users.noreply.github.com>
2020-12-21 09:11:07 +0500

    Добавлен класс Cat, добавлена аннотация @Scope("prototype"). Надо доделать сравнение.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat beanCat = (Cat) applicationContext.getBean("cat");//передаем в качестве параметра название bean
+        System.out.println("From bean " + beanCat.getMessage());
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        //System.out.println("Bean Hellow World" + helloWorld);
+
+        System.out.println("Bean cat" + cat);
+        System.out.println("Bean cat2" + cat2);
+
+        //System.out.println(helloWorld == cat);
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+public class Cat {
+    public String name;
+
+
+
+    public String getMessage() {
+        return name;
+    }
+
+    public void setMessage(String name) {
+        this.name = name;
+    }
+}
